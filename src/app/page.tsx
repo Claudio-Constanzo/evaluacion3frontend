79c104bb7315caa8d6eea3e2e36e4514df35bdfd
Claudio-Constanzo <claudio.constanzo02@gmail.com>
2025-07-05 22:42:57 -0400

    creacion de pagina principal
    se integran useState y useEffect para almacenar en el localstorage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,3 +2,20 @@
 import { useEffect, useState } from 'react';
 import { Persona } from './interfaces/Persona';
 import FormularioPersona from './componentes/FormularioPersona';
+
+export default function Page() {
+  const [nombreActual, setNombreActual] = useState('');
+  const [personas, setPersonas] = useState<Persona[]>([]);
+  const [personaSeleccionada, setPersonaSeleccionada] = useState<Persona | null>(null);
+  const [indiceEditar, setIndiceEditar] = useState<number | null>(null);
+
+  useEffect(() => {
+    const datosGuardados = localStorage.getItem('personas');
+    if (datosGuardados) {
+      setPersonas(JSON.parse(datosGuardados));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('personas', JSON.stringify(personas));
+  }, [personas]);
