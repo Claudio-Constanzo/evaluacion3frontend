b849be4703535af20d2a205f28f37fc7addc63aa
Claudio-Constanzo <claudio.constanzo02@gmail.com>
2025-07-12 21:00:22 -0400

    configuracion de credenciales y firebase
    se ingresó el proyecto al codigo para futuro funcionamiento y CRUD

--- src/Credenciales.js
+++ src/Credenciales.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCuio62EKeBCDgMibheAVuVIrzHbxexNBU",
   authDomain: "evaluacion-4-73cae.firebaseapp.com",
   projectId: "evaluacion-4-73cae",
-  storageBucket: "evaluacion-4-73cae.firebasestorage.app",
+  storageBucket: "evaluacion-4-73cae.firebasestorage.appspot.com",
   messagingSenderId: "693305880675",
   appId: "1:693305880675:web:d88044f0ef51dd838d0856",
   measurementId: "G-RBS54FDZW8"
